bd5978083cdd096fa1f83e43a4e8dbb36cd5f2a7
Cynerva <cynerva@gmail.com>
2013-09-01 21:19:04 -0500

    Clean up the lunar heightmap

--- states/lunar_surface.py
+++ states/lunar_surface.py
@@ -1,4 +1,5 @@
 import math
+import random
 import pygame
 from Box2D import *
 
@@ -6,31 +7,33 @@ import backgrounds
 from camera import Camera
 from player import Player
 from heightmap import heightmap_1d
+from config import screen_height
 from debug import draw_body
 
 
 class LunarSurface(object):
     def __init__(self):
+        """ Good god what is this even """
         self.background = backgrounds.LunarSurface()
         self.camera = Camera()
         self.world = b2World(gravity=(0, -100), doSleep=True)
  
-        heightmap = heightmap_1d(8)
-        ground_shapes = []
-        heightmap[0] *= 128.0
-        for x in range(1, 256):
+        heightmap = heightmap_1d(12)
+        for x in range(4096):
             heightmap[x] *= 128.0
-            heightmap[x] += (((x - 128.0) / 128.0) ** 2 - 1.0) * 1024.0
-            left = heightmap[x - 1]
-            right = heightmap[x]
-            ground_shapes.append(b2PolygonShape(
-                vertices=(
-                    (x*10-10, left),
-                    (x*10-10, left-20),
-                    (x*10, right-20),
-                    (x*10, right)
-                )
-            ))
+        for x in range(1024, 4096):
+            heightmap[x] += (((x - 2048) / 1024.0) ** 2.0 - 1.0) * 1024.0
+
+        ground_shapes = []
+        for x in range(4096/16-1):
+            left = heightmap[x*16]
+            right = heightmap[x*16+16]
+            ground_shapes.append(b2PolygonShape(vertices=(
+                (x * 16, left),
+                (x * 16, left - 16),
+                (x * 16 + 16, right - 16),
+                (x * 16 + 16, right)
+            )))
         self.ground = self.world.CreateStaticBody(
             position=(0, 0),
             shapes=ground_shapes
@@ -48,5 +51,5 @@ class LunarSurface(object):
 
     def render(self, screen):
         self.background.render(screen, self.camera)
-        draw_body(self.ground, screen, self.camera)
         self.player.render(screen, self.camera)
+        draw_body(self.ground, screen, self.camera)
