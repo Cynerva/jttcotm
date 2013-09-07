c2f4ed7af08092a48ae7f68c798d50fb4cf4998e
Cynerva <cynerva@gmail.com>
2013-09-07 18:23:07 -0500

    Tweak world gen book spawn rate

--- states/worldgen.py
+++ states/worldgen.py
@@ -189,7 +189,7 @@ class CaveGenState(object):
             pos = (self.pos[0], self.pos[1] - width + 2.0)
             event = EndEvent(pos)
             self.world.add_entity(event, pos)
-        elif random.random() < 0.5:
+        elif random.random() < 1.0 / 3.0:
             text = random.choice(open("data/text/books.txt").readlines()).strip()
             pos = (self.pos[0], self.pos[1] - width + 2.0)
             event = Book(text, pos)
